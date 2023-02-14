80eee451a682a6199a7b0585115755fe68f6e7e5
LeTismey <124163690+NCHM62@users.noreply.github.com>
2023-02-14 20:51:57 +0100

    Update livre.h

--- livre.h
+++ livre.h
@@ -1,20 +1,3 @@
-#ifndef LIVRE_H   //compilation conditionnelle
-#define LIVRE_H
-
-#include "es.h"
-
-# define MAX 20
-# define MAX_TITRE 60
-# define MAX_CODE 6
-
-# define K_MaxTit MAX_TITRE
-# define K_MaxAut MAX
-# define K_MaxEdit MAX
-# define K_MaxEmp MAX
-# define K_MaxCode MAX_CODE
-
-typedef char T_Titre[K_MaxTit];
-typedef char T_Aut[K_MaxAut];
 typedef char T_Edit[K_MaxEdit];
 typedef char T_Emp[K_MaxEmp]; //utilisez d'abord ce type puis, remplacez le par celui ci dessous ()
 typedef char T_Code[K_MaxCode];
@@ -22,9 +5,9 @@ typedef int T_annee;
 
 
 // à utiliser pour gérer l'emprunt d'un livre
-
-typedef enum T_Jour {lu,ma,me,je,ve,sa,di};
-typedef enum T_Mois {janv,fevr,mars,avri,mai,juin,juil,aout,sept,octo,nove,dece};
+/*
+typedef enum  {lu,ma,me,je,ve,sa,di}T_Jour;
+typedef enum  {janv,fevr,mars,avri,mai,juin,juil,aout,sept,octo,nove,dece}T_Mois;
 typedef struct
 {
 char nomemprunteur[50];
@@ -33,7 +16,7 @@ int ledate;
 T_Mois lemois;
 int lannee
 }T_Emp ;
-
+*/
 
 
 // debut de TP, on utilisera les 2 champs titre et auteur
