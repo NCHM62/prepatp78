7e855873fbad315688526c505643cf687807e326
LeTismey <124163690+NCHM62@users.noreply.github.com>
2023-02-14 20:51:05 +0100

    Update biblio.c

--- biblio.c
+++ biblio.c
@@ -1,37 +1,15 @@
 # include "biblio.h"
 
-int chercherLivreaut(T_Bibliotheque  *ptrB)
-{
-
-	int i;
-while( strcmp((ptrB->etagere[i]).titre),ptrL->auteur) != 0)
-	i++;
-if(strcmp((ptrB->etagere[i]).titre),ptrL->auteur) == 0)
-	return 1;
-else
- 	return 0;	
-	
-	
-}
-// il faut remplacer le livre à supprimer par le dernier livre
-
-int supprimer (T_Bibliotheque  *ptrB)
-{ while( strcmp((ptrB->etagere[i]).titre),ptrL->titre) != 0)
-	i++;
-if(strcmp((ptrB->etagere[i]).titre),ptrL->titre) == 0)   
- *ptrB=*T_tabloDeLivres[ptrB->nbLivres];
-}
-
-
 
 int  chercherLivre( T_Bibliotheque  *ptrB, char* titre)
-{ int i=0, compteur=0;
+{ 
+int i=0, compteur=0;
 while( i < ptrB->nbLivres){
 	
-	if(strcmp((ptrB->etagere)[i].titre),titre) == 0)
+	if((strcmp(((ptrB->etagere)[i].titre),titre) == 0))
 		compteur++;
 	i++;
-}
+};
 return compteur;
 }
 
@@ -77,5 +55,3 @@ if(ptrB->nbLivres==0)
 		return 1;
 		}
 }
-
-
