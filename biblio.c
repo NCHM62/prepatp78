4dd01bee82c49b9c6d6438251c4a29a4dd6d32fa
LeTismey <124163690+NCHM62@users.noreply.github.com>
2023-03-02 23:07:36 +0100

    Update biblio.c

--- biblio.c
+++ biblio.c
@@ -55,3 +55,33 @@ if(ptrB->nbLivres==0)
 		return 1;
 		}
 }
+
+
+int  chercherLivreaut( T_Bibliotheque  *ptrB, char* auteur)
+{ 
+	int i=0, compteur=0;
+	while( i < ptrB->nbLivres){
+	
+		if((strcmp(((ptrB->etagere)[i].auteur),auteur) == 0))
+			compteur++;
+		i++;
+		}
+	return compteur;
+}
+
+int supprimerLivre(T_Bibliotheque  *ptrB, char* titre)
+{
+	int i=0, compteur=0;
+	while( i < ptrB->nbLivres){
+	
+		if((strcmp(((ptrB->etagere)[i].titre),titre) == 0))
+		{
+			compteur++;
+			/*aux = (ptrB->etagere)[i].titre
+			(ptrB->etagere)[i] = (ptrB->etagere)[i + 1];*/
+			//Remplacer le livre supprimé (position i) par le livre (position : i+1) [A terminer]
+		}													
+		i++;
+	}
+	return compteur;
+}
