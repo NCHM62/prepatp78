3a7505375cdc392e02a7fb6d72eff160f6318415
LeTismey <124163690+NCHM62@users.noreply.github.com>
2023-03-04 12:01:27 +0100

    Update biblio.c

--- biblio.c
+++ biblio.c
@@ -3,14 +3,14 @@
 
 int  chercherLivre( T_Bibliotheque  *ptrB, char* titre)
 { 
-int i=0, compteur=0;
-while( i < ptrB->nbLivres){
+	int i=0, compteur=0;
+	while( i < ptrB->nbLivres){
 	
-	if((strcmp(((ptrB->etagere)[i].titre),titre) == 0))
-		compteur++;
-	i++;
-};
-return compteur;
+		if((strcmp(((ptrB->etagere)[i].titre),titre) == 0))
+			compteur++;
+		i++;
+		}
+	return compteur;
 }
 
 
@@ -24,38 +24,31 @@ ptrB->nbLivres=0;
 
 int ajouterLivre(T_Bibliotheque  *ptrB)
 {
-if (ptrB->nbLivres<CAPACITE_BIBLIO)// reste t il de la place?
-	{
-	saisirLivre(&(ptrB->etagere[ptrB->nbLivres]));
-	ptrB->nbLivres++;
-	return 1;
-	}
-	
-	
-return 0;
-	
+	if (ptrB->nbLivres<CAPACITE_BIBLIO)// reste t il de la place?
+		{
+		saisirLivre(&(ptrB->etagere[ptrB->nbLivres]));
+		ptrB->nbLivres++;
+		return 1;
+		}
+	return 0;
 }
 
 
 int  afficherBibliotheque(const T_Bibliotheque  *ptrB)
 {
-int i;
-if(ptrB->nbLivres==0)
-	return 0;
-	else
-		{
-		for(i=0;i<ptrB->nbLivres;i++)
+	int i;
+	if(ptrB->nbLivres==0)
+		return 0;
+		else
 			{
-			afficherLivre( &(ptrB->etagere[i])  );
+			for(i=0;i<ptrB->nbLivres;i++)
+				{
+				afficherLivre( &(ptrB->etagere[i])  );
 			
-			}
-
-
-		
+				}
 		return 1;
 		}
 }
-
 
 int  chercherLivreaut( T_Bibliotheque  *ptrB, char* auteur)
 { 
@@ -71,51 +64,20 @@ int  chercherLivreaut( T_Bibliotheque  *ptrB, char* auteur)
 
 int supprimerLivre(T_Bibliotheque  *ptrB, char* titre)
 {
-	int i=0, compteur=0;
-	while( i < ptrB->nbLivres){
-	
-		if((strcmp(((ptrB->etagere)[i].titre),titre) == 0))
-		{
-			compteur++;
-			/*aux = (ptrB->etagere)[i].titre
-			(ptrB->etagere)[i] = (ptrB->etagere)[i + 1];*/
-			//Remplacer le livre supprimé (position i) par le livre (position : i+1) [A terminer]
-		}													
-		i++;
-	}
-	return compteur;
-}
-
-/*
-int supprimerLivre(T_Bibliotheque *ptrB) [code de Meryem]
-{
-
-    int i = 0, cmp;
-    char M_Titre[MAX_TITRE];
-    printf("\n Quel livre?");
-    lireChaine("\n Saisir le titre du livre pour une supression. ", M_Titre, MAX_TITRE);
-    while (i != ptrB->nbLivres + 1)
-    {
-        cmp = strcmp(M_Titre, (ptrB->etagere[i].titre));
-        if (cmp == 0 && i != (ptrB->nbLivres) + 1)
-        {
-            while (i != ptrB->nbLivres)
-            {
-
-                ptrB->etagere[i] = ptrB->etagere[i + 1];
-                i++;
+    int i = 0, nb_suppressions = 0;
+    while (i < ptrB->nbLivres) {
+        if (strcmp(ptrB->etagere[i].titre, titre) == 0) {
+            // Déplacer tous les livres suivants d'une position vers la gauche
+            for (int j = i; j < ptrB->nbLivres - 1; j++) {
+                ptrB->etagere[j] = ptrB->etagere[j + 1];
             }
-            (ptrB->nbLivres)--;
-            return 1;
+            // Réduire le nombre total de livres dans l'étagère
+            ptrB->nbLivres--;
+            nb_suppressions++;
+        } else {
+            i++;
         }
-        if (cmp == 0 && i == (ptrB->nbLivres) + 1)
-        {
-
-            ptrB->etagere[i] = ptrB->etagere[i + 1];
-            (ptrB->nbLivres)--;
-        }
-        i++;
     }
-    return 0;
+    return nb_suppressions;
 }
-*/
+
