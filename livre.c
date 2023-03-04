cbb6c5dcc8acf9609e9d54b7a375e343992c487e
LeTismey <124163690+NCHM62@users.noreply.github.com>
2023-03-04 12:02:42 +0100

    Update livre.c

--- livre.c
+++ livre.c
@@ -3,15 +3,17 @@
 
 void saisirLivre(T_livre * ptrL)
 {
-lireChaine("TITRE :", (ptrL->titre), MAX_TITRE );
-lireChaine("AUTEUR :", (ptrL->auteur ), MAX);
+lireChaine("\nTITRE : ", (ptrL->titre), MAX_TITRE );
+lireChaine("AUTEUR : ", (ptrL->auteur), MAX);
+/*lireChaine("EDITEUR : ", (ptrL->editeur), MAX);
+lireChaine("ANNEE : ", (ptrL->annee), MAX);*/
 }
 
 void afficherLivre(const T_livre *ptrL)
 {
-afficherChaine("TITRE :", (ptrL->titre));
-printf(" - ");
-afficherChaine("AUTEUR :", (ptrL->auteur ));
+afficherChaine("\nTITRE :", (ptrL->titre));
+afficherChaine("\nAUTEUR :", (ptrL->auteur));
+/*lireChaine("\nEDITEUR : ", (ptrL->editeur));
+lireChaine("\nANNEE : ", (ptrL->annee));*/
 printf("\n");
 }
-
