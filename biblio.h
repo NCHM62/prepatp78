a8199de52c72c09486432eb05aadf038d089e52b
LeTismey <124163690+NCHM62@users.noreply.github.com>
2023-03-06 12:12:56 +0100

    Update biblio.h

--- biblio.h
+++ biblio.h
@@ -1,3 +1,9 @@
+#ifndef BIBLIO_H   //compilation conditionnelle
+#define BIBLIO_H
+
+
+#include "livre.h"
+
 #define CAPACITE_BIBLIO 10 // nb maximum de livres ds la bibliotheque
 
 typedef  T_livre 	T_tabloDeLivres[CAPACITE_BIBLIO];
@@ -13,17 +19,19 @@ typedef struct
 
 
 //prototypes
+void lireDateSysteme(T_Emp *E);
 void init (T_Bibliotheque *ptrB);
 int ajouterLivre(T_Bibliotheque  *ptrB);
-int  afficherBibliotheque(const T_Bibliotheque  *ptrB);
 void sauvegarde(T_Bibliotheque *ptrB);
 void chargement(T_Bibliotheque *ptrB);
+int  afficherBibliotheque(const T_Bibliotheque  *ptrB);
 int  chercherLivre( T_Bibliotheque  *ptrB, char* titre);
-int supprimerLivre(T_Bibliotheque  *ptrB, T_Titre tit);
+int  chercherLivreaut( T_Bibliotheque  *ptrB, char* auteur);
+int supprimerLivre(T_Bibliotheque  *ptrB, char* titre);
 int Emprunt_Livre(T_Bibliotheque *ptrB);
 int Rendre_Livre(T_Bibliotheque *ptrB);
-int rechercherParIndice(const T_Bibliotheque  *ptrB, T_Titre tit);
+char *format_tri(char titre[MAX_TITRE], char temp1[MAX_TITRE]);
 int tri_Titre(T_Bibliotheque *ptrB);
 int tri_auteur(T_Bibliotheque *ptrB);
-int tri_annee(T_bibliotheque *ptrB);
+//int Rendre_Livre(T_Bibliotheque *ptrB)int Rendre_Livre(T_Bibliotheque *ptrB);
 #endif
