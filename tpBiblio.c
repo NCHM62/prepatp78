dee69475fd74ae4c0541e3bea0520aba4d5e5194
CrazyMery <124631105+CrazyMery@users.noreply.github.com>
2023-03-06 08:16:47 +0100

    Update tpBiblio.c

--- tpBiblio.c
+++ tpBiblio.c
@@ -15,13 +15,13 @@ printf("\n 5 - Supprimer un livre de la bibliothèque");
 
 // au programme du TP8 :
 // ajouter le champ emprunteur à votre structure T_Livre
-/*
+
 printf("\n 6 - Emprunter un livre de la bibliothèque");
 printf("\n 7 - Restituer/rendre un livre de la bibliothèque");
 printf("\n 8 - Trier les livres (par titre)");
 printf("\n 9 - Trier les livres (par auteur)");
 printf("\n 10 - Trier les livres (par année)");
-*/
+
 // si les 5 choix (6-10) sont bien codés, changez le type T_Emp et remplacez-le par la structure T_Emp visible dans livre.h
 // vous pourrez alors faire les menus 11,12,etc...
 // printf("\n 11- Lister les livres disponibles "); 
@@ -62,39 +62,59 @@ switch(chx)
 					printf("\nLa bibliotheque est vide\n");
 
 			break;	
-	case 3: 
-		printf("\nSaisir le titre du livre : ");
-		lire(titre,60);
-		reponse=chercherLivre(&B,titre);
+
+	
+		case 3: reponse=chercherLivre(&B);
 			
-		if (reponse>=1)
-				printf ("\nLe livre est dans la biblio, en %d exemplaire(s)",reponse);
-		else
-				printf("\nLe livre n'est pas dans la biblio");
-		break;
-	case 4:
-		printf("\nSaisir le nom de l'auteur : ");
-		lire(auteur,60);
-		reponse=chercherLivreaut(&B,auteur);
-		
-		if (reponse>=1)
-				printf ("\nLe ou les livre(s) de l'auteur sont dans la biblio, en %d exemplaire(s)\n",reponse);
-		else
-				printf("\nLe ou les livres de l'auteur ne sont pas dans la biblio\n");
+			if (reponse>=1)
+				printf ("le livre est dans la biblio, en %d exemplaire(s)",reponse);
+			else
+				printf("le livre n'est pas dans la biblio");
+			break;	
+			
+	case 4:  afficherLivreAuteur(&B);
 		
 			break;
 	
-	case 5:
-			/*supprimerLivre(&B);*/
-			printf("\nSaisir le titre du livre : ");
-			lire(titre,60);
-			reponse=supprimerLivre(&B, titre);
-			if (reponse>0)
-				printf ("Le livre a été supprimé %d fois dans la bibliothéque", reponse);
-			else if(reponse==0)
-				printf("Le livre n'a pas été supprimé");
+	case 5:lireChaine("Donnez le titre du livre a  supprimer :", titre , 100);
+		reponse = supprimerLivre(&B, titre);
+		if(reponse==-1)
+			printf(" aucun livre trouvee");
+		else
+			printf("le Livre  est supprimee");
 			break;
-	
+	case 6:  reponse = Emprunt_Livre(&B);
+				if (reponse == 1)
+					printf ("\n Emprunt est effectue !");
+				else if (reponse == 0)
+					printf ("\n Aucune correspondance code pour ce livre !");
+				else
+					printf("\nLe livre est deja  empruntee !");
+		
+			break;
+			
+	case 7:  	reponse = Rendre_Livre(&B);
+				if (reponse == 1)
+					printf ("\n Rendu effectuee");
+				else if (reponse == 0)
+					printf ("\n Aucune correspondance code pour ce livre !");
+				else
+					printf("\nCe livre est deja  rendu");
+		
+			break;
+	case 8:  	reponse = tri_Titre(&B);
+				if (reponse == 1)
+					printf ("\n Tri effectuee");
+				else
+					printf ("Erreur");
+		
+			break;
+	case 9:  reponse=tri_auteur(&B);
+		
+			break;
+	case 10:  reponse=tri_annee(&B);
+		
+			break;
 	
 	
 	
